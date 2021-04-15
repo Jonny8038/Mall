13b4fa5b8d89b28aa013bd4dd7aca70116e1c21e
Jonny <13068868038@163.com>
2021-04-15 19:48:26 +0800

    .

--- src/main.js
+++ src/main.js
@@ -2,8 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios'
+import './assets/global.css'
+import '../src/assets/font/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
+
+Vue.component('tree-table', TreeTable);
+Vue.filter('dateFromat', function (time) {
+  const dt = new Date(time)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
+
+
+axios.defaults.baseURL = 'http://8.140.111.133:8889/api/private/v1/'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
+
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,52 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import User from '../components/user/User.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Rolse.vue'
+import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/params.vue'
+import List from '../components/goods/List.vue'
+import Add from '../components/goods/Add.vue'
 
-Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+Vue.use(VueRouter);
+
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+  // mode: 'history',
+  base: process.env.BASE_URL,  
+  routes: [
+    { path: '/', redirect: '/login' },
+    { path: '/login', component: Login },
+    {
+      path: '/home',
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        { path: '/welcome', component: Welcome },
+        { path: '/users', component: User },
+        { path: '/rights', component: Rights },
+        { path: '/roles', component: Roles },
+        { path: '/categories', component: Cate },
+        { path: '/params', component: Params },
+        { path: '/goods', component: List },
+        { path: '/goods/add', component: Add },
+      ]
+    },
+  ]
 })
+
+router.beforeEach(function (to, from, next) {
+  if (to.path === '/login') return next();
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login')
+  next()
+})
+
+
+
 
 export default router
